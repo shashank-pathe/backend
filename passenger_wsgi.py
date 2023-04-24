0cec9975a4caee779bb62abd3259134a46e8258d
Shashank <96712140+shashank-pathe@users.noreply.github.com>
2023-04-24 19:52:33 +0530

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,13 @@
-from backend.wsgi import application
+from backend.wsgi import applicationimport os
+import sys
+
+# Add the directory containing your Django project to the Python path
+django_project_path = "/home/shashankpathe1/realyou"
+sys.path.append(django_project_path)
+
+# Set the environment variable to tell Django which settings module to use
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+# Set the application object
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
